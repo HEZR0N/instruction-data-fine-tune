56e37d9513dc0843383cfe652cdb7ae921051b46
HEZR0N <99786488+HEZR0N@users.noreply.github.com>
2024-05-05 16:33:43 -0500

    Update add_instructions.py

--- add_instructions.py
+++ add_instructions.py
@@ -3,25 +3,37 @@ from datasets import load_from_disk
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # load dataset for instruction_tuning
-dataset_2 = load_from_disk("./news_dataset")
+dataset = load_from_disk("./news_dataset")
 
+# add the classification labels to the dataset, which are currently just represented by numbers
 def add_label_name(data):
     data['label_name'] = {0:'World', 1:'Sports', 2:'Business', 3:'Science'}[data['label']]
     return data
 
+# append the first type of instruction to data
 def add_extra_instr_1(data):
     data['text'] = 'Answer with one word. Categorize the following passage with one of the following options: World, Sports, Business, or Science: ' + data['text']
     return data
 
+# append the second type of instruction to data
 def add_extra_instr_2(data):
-    data['text'] = 'Answer with one word. Of the subjects, World, Sports, Business, and Science, Which subject would best describe the following passage: ' + data['text']
+    data['text'] = 'Answer with one word. Of the subjects, World, Sports, Business, and Science, which subject would best describe the following passage: ' + data['text']
     return data
 
+# call label function
+dataset = dataset.map(add_label_name)
 
-dataset_2 = dataset_2.map(add_label_name)
-dataset_2 = dataset_2.map(add_extra_instr_1)
-dataset_2.save_to_disk("./instruction_1_news_dataset")
-dataset_3 = dataset_2.map(add_extra_instr_2)
-dataset_3.save_to_disk("./instruction_2_news_dataset")
+# Ideally, there would just be two columns, one for each instruction for each elemtent, but instead I made 2 datasets for because each element from the dataset should
+# work with 2 different types of instructions
+
+# Save the first version of the dataset where each element has the first type of instruction
+dataset_1 = dataset.map(add_extra_instr_1)
+dataset_1.save_to_disk("./instruction_1_news_dataset")
+
+# Save the second version of the dataset where each element has the second type of instruction
+dataset_2 = dataset.map(add_extra_instr_2)
+dataset_2.save_to_disk("./instruction_2_news_dataset")
+
+# show the two types of instructions for the first element of the dataset
+print(dataset_1[0])
 print(dataset_2[0])
-print(dataset_3[0])
