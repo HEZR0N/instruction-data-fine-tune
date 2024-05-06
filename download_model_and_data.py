3398f55c8453d6554d48e5784a2a1079acf3e41c
HEZR0N <99786488+HEZR0N@users.noreply.github.com>
2024-05-06 02:03:36 -0500

    Update download_model_and_data.py

--- download_model_and_data.py
+++ download_model_and_data.py
@@ -4,20 +4,21 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # load model and tokenizer
 access_token="hf_token"
-model_path = "databricks/dolly-v2-3b"
+model_path = "unsloth/OpenHermes-2.5-Mistral-7B-bnb-4bit"
 model = AutoModelForCausalLM.from_pretrained(model_path, token=access_token)
-model.save_pretrained("./dolly_pretrained_model")
+model.save_pretrained("./openhermes_pretrained_model")
 #print(model)
 
 # initialize tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_path, token=access_token)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.save_pretrained("./dolly_tokenizer")
+#tokenizer.save_pretrained("./openhermes_tokenizer")
+tokenizer.save_pretrained("./openhermes_pretrained_model")
 
 # load default dataset that the model was trained on
-dataset = load_dataset("databricks/databricks-dolly-15k")
-dataset = dataset['train'].select(range(15000))
-dataset.save_to_disk("./dolly_dataset")
+# dataset = load_dataset("teknium/OpenHermes-2.5", split='train[:1%]', keep_in_memory=True)
+dataset = load_dataset("jondurbin/airoboros-2.2", split='train[:10%]', keep_in_memory=True)
+dataset.save_to_disk("./openhermes_dataset")
 print(dataset)
 print(dataset[0])
 
